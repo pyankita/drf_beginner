4735eb5ec35a6fef3435f9881538d431b9516ad4
pyankita <pyankitaghartimagar@gmail.com>
2026-05-10 18:35:54 +0545

    installed dependencies in the environment

--- drf_beginner/models.py
+++ drf_beginner/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    pass
+    name=models.charField(max_length=100,null=True,blank=True)
+    price=models.FloatField(null=True,blank=True)
+
+    def __str__(self):
+        self.name
