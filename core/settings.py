6aaef87e8da8b88ce6875ecc300cfc840c2038fd
pyankita <pyankitaghartimagar@gmail.com>
2026-06-20 18:21:26 +0545

    install the rest_framework and added the dunder method

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "drf_beginner",
-    
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
--- drf_beginner/models.py
+++ drf_beginner/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name=models.charField(max_length=100,null=True,blank=True)
+    name=models.CharField(max_length=100,null=True,blank=True)
     price=models.FloatField(null=True,blank=True)
 
     def __str__(self):
-        self.name
+        return self.name
